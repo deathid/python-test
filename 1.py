312e4660881d6ace20cab5a1e841716271abe38b
ZWF <deathid@icloud.com>
2018-12-14 16:03:48 +0800

    字符串

--- 1.py
+++ 1.py
@@ -2,3 +2,14 @@ a = True
 b = not True
 
 print(1<2 and b==True)
+
+c = 0
+
+for i in range(1,101):
+    c = c + i
+
+print(c)
+
+print("he is a 'good' man")
+print("he said","I\'m \"yours!\"","\n","\\\\_v  _//","\n""Stay hungry,\nStay foolish.\n       --Steve Jobs\n"\
+      "*\n""**\n""***\n""**\n""*")
